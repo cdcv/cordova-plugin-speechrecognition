e2a94cba5daa6211a92fe9c98bdf81a948b2be90
Christopher deCharms <decharms@gmail.com>
2019-01-11 11:17:43 +0700

    Added ;

--- src/android/com/pbakondy/SpeechRecognition.java
+++ src/android/com/pbakondy/SpeechRecognition.java
@@ -167,8 +167,8 @@ public class SpeechRecognition extends CordovaPlugin {
     final Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 
         // CdC Additions
-        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, "4000")
-        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, "4000")
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, "4000");
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, "4000");
 
     intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
             RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
